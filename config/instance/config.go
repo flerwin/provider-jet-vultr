eb6636976d5d6fe39e95caaae41c0965549a4892
Antonio Broughton <antonio@flerwin.com>
2022-11-30 20:50:11 +1300

    to appease the linting gods

--- config/instance/config.go
+++ config/instance/config.go
@@ -2,6 +2,7 @@ package instance
 
 import "github.com/upbound/upjet/pkg/config"
 
+// Configure configures the instance package.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("vultr_instance", func(r *config.Resource) {
 		r.ExternalName = config.IdentifierFromProvider
--- config/objectstorage/config.go
+++ config/objectstorage/config.go
@@ -2,6 +2,7 @@ package objectstorage
 
 import "github.com/upbound/upjet/pkg/config"
 
+// Configure configures the objectstorage package.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("vultr_object_storage", func(r *config.Resource) {
 		r.ExternalName = config.IdentifierFromProvider
